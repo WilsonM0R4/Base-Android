559203b95a9446205b9b4324e4af4fd2fee1fe51
serfarfan <serfar260@hotmail.com>
2016-07-07 14:44:52 -0500

    Delete summary screen in QR code.

--- app/src/main/java/com/allem/alleminmotion/visacheckout/QRScanActivity.java
+++ app/src/main/java/com/allem/alleminmotion/visacheckout/QRScanActivity.java
@@ -37,7 +37,10 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 
 
-public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate.InflateReadyListener   {
+public class QRScanActivity extends FrontBackAnimate{// implements FrontBackAnimate.InflateReadyListener
+
+    //**************GLOBAL ATTRIBUTES************
+
     private static final String TAG = "QRScanner" ;
     private ImageView iv_qrcode;
     private ArrayList<NameValuePair> postValues;
@@ -47,14 +50,15 @@ public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate
     private ProgressBar pb_create;
     private String currency, reference;
 
-
+    //************OVERRIDE AND SUBSCRIBE METHODS
 
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
         MyBus.getInstance().register(this);
+        postValues = new ArrayList<>();
         //setContentView(R.layout.activity_qrscan);
-        setView(R.layout.activity_qrscan, this);
+        //setView(R.layout.activity_qrscan, this);
         if (checkLogin()) {
             openScan();
         }
@@ -71,14 +75,77 @@ public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate
         super.onPause();
     }
 
+    @Subscribe
+    public void onAsyncTaskResult(AsyncTaskSoapObjectResultEvent event) {
+        //setWaitinUI(false);
+        switch(event.getCodeRequest()) {
+            case Constants.ACTIVITY_PAY_DETALLE:
+                if (event.getResult() != null) {
+
+                    solicitud = PayParsers.toSolicitudCobro(QRScanActivity.this, event.getResult());
+                    //vendor.setText(solicitud.razonSocial);
+                    BigDecimal beforeTax = solicitud.baseDevolucion;
+                    BigDecimal tmpBase = beforeTax.setScale(2, BigDecimal.ROUND_HALF_UP);
+                    BigDecimal adjbeforeTax  = tmpBase.setScale(0, BigDecimal.ROUND_HALF_UP);
+                    //itemsValue.setText("$ " + adjbeforeTax);
+
+                    BigDecimal tmpTotal = solicitud.valor.setScale(2, BigDecimal.ROUND_HALF_UP);
+                    BigDecimal adjTotal = tmpTotal.setScale(0, BigDecimal.ROUND_HALF_UP);
+                    //total.setText("$ " + adjTotal);
+
+                    BigDecimal tax = solicitud.iva.multiply(solicitud.baseDevolucion).divide(new BigDecimal("100"));
+                    BigDecimal tmpIVA = tax.setScale(2, BigDecimal.ROUND_HALF_UP);
+                    BigDecimal adjIVA = tmpIVA.setScale(0, BigDecimal.ROUND_HALF_UP);
+                    //taxes.setText("$  " + adjIVA);
+                    currency = solicitud.moneda;
+                    reference = solicitud.referencia;
+
+                    //Testing
+                    Intent intent2 = new Intent(QRScanActivity.this, OnepocketPurchaseActivity.class);
+                    Bundle bundle = new Bundle();
+                    String data = "{\"businessName\":\"" + solicitud.razonSocial +//vendor.getText()
+                            "\",\"purchaseValue\":\"" + ("$ " + adjTotal).toString().replace("$", "").trim() +//total.getText()
+                            "\",\"taxValue\":\"" + ("$  " + adjIVA).toString().replace("$", "").trim() +
+                            "\",\"devolutionBasisTax\":\"" + ("$ " + adjbeforeTax).toString().replace("$", "").trim() +
+                            "\",\"codeISO\":\"" + currency +
+                            "\",\"reference\":\"" + reference +
+                            "\"}";
+                    OneTransaction transaction = new OneTransaction(data,
+                            OPKConstants.TYPE_QRCODE,
+                            ((VisaCheckoutApp)QRScanActivity.this.getApplication()).getIdSession(),
+                            Constants.getUser(QRScanActivity.this).nombre,
+                            Constants.getUser(QRScanActivity.this).apellido,
+                            Constants.getUser(QRScanActivity.this).email,
+                            ((VisaCheckoutApp) getApplication()).getRawPassword(),
+                            Constants.getUser(QRScanActivity.this).idCuenta);
+
+                    bundle.putParcelable(OPKConstants.EXTRA_PAYMENT, transaction);
+                    intent2.putExtras(bundle);
+                    startActivity(intent2);
+
+                    finish();
+                } else {
+
+                    Log.e(TAG, event.getFaultString());
+                    Toast.makeText(QRScanActivity.this, event.getFaultString(), Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+            }
+        }
+
+    //***************PROPER METHODS************
+
     public void onMenu(View view) {
         animate();
     }
 
     private Bitmap drawQRCode(String data,int size) throws Exception
     {
-        Bitmap bitmap =null;
-        QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(data, null, Contents.Type.TEXT, BarcodeFormat.QR_CODE.toString(),size);
+        Bitmap bitmap = null;
+        QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(data, null, Contents.Type.TEXT,
+                BarcodeFormat.QR_CODE.toString(),
+                size);
 
         try {
 
@@ -92,7 +159,7 @@ public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate
 
     private void obtenerTicket(String idSolicitud) {
         if (Util.hasInternetConnectivity(this)){
-            setWaitinUI(true);
+            //setWaitinUI(true);
             Log.d(TAG,Constants.KEY_ID_SOLICITUD+":"+idSolicitud);
             postValues.add(new BasicNameValuePair(Constants.KEY_ID_SOLICITUD,idSolicitud));
             AsyncSoapObject.getInstance(Constants.getWSDL(), Constants.NAMESPACE_ALLEM,
@@ -102,44 +169,8 @@ public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate
         }
     }
 
-
-    @Subscribe
-    public void onAsyncTaskResult(AsyncTaskSoapObjectResultEvent event) {
-        setWaitinUI(false);
-        switch(event.getCodeRequest()) {
-            case Constants.ACTIVITY_PAY_DETALLE:
-                if (event.getResult() != null) {
-
-                    solicitud = PayParsers.toSolicitudCobro(QRScanActivity.this, event.getResult());
-                    vendor.setText(solicitud.razonSocial);
-                    BigDecimal beforeTax = solicitud.baseDevolucion;
-                    BigDecimal tmpBase = beforeTax.setScale(2, BigDecimal.ROUND_HALF_UP);
-                    BigDecimal adjbeforeTax  = tmpBase.setScale(0, BigDecimal.ROUND_HALF_UP);
-                    itemsValue.setText("$ " + adjbeforeTax);
-
-                    BigDecimal tmpTotal = solicitud.valor.setScale(2, BigDecimal.ROUND_HALF_UP);
-                    BigDecimal adjTotal = tmpTotal.setScale(0, BigDecimal.ROUND_HALF_UP);
-                    total.setText("$ " + adjTotal);
-
-                    BigDecimal tax = solicitud.iva.multiply(solicitud.baseDevolucion).divide(new BigDecimal("100"));
-                    BigDecimal tmpIVA = tax.setScale(2, BigDecimal.ROUND_HALF_UP);
-                    BigDecimal adjIVA = tmpIVA.setScale(0, BigDecimal.ROUND_HALF_UP);
-                    taxes.setText("$  " + adjIVA);
-
-                    currency = solicitud.moneda;
-                    reference = solicitud.referencia;
-
-                } else {
-
-                    Log.e(TAG, event.getFaultString());
-                    Toast.makeText(QRScanActivity.this, event.getFaultString(), Toast.LENGTH_LONG).show();
-                    finish();
-                }
-                break;
-            }
-        }
-
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 
         if (scanResult != null) {
@@ -148,8 +179,9 @@ public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate
             if (resultCode == -1) {
                 Log.d("resultado QR", id);
                 try {
-                    iv_qrcode.setImageBitmap(drawQRCode(id, GraphicsUtils.getWindowSize(this).x));
+                    //iv_qrcode.setImageBitmap(drawQRCode(id, GraphicsUtils.getWindowSize(this).x));
                     obtenerTicket(id);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -164,21 +196,10 @@ public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate
             openScan();
         }
 
-
-        /*
-
-        if (resultCode == RESULT_OK) {
-//            Intent intent = new Intent(this, MainActivity.class);
-//            startActivity(intent);
-            setResult(resultCode);
-            finish();
-        } else {
-            finish();
-        }*/
-        // else continue with any other code you need in the method
     }
 
     private void openScan(){
+        Log.d("Entra a","openScan");
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setCaptureActivity(CustomQrActivity.class);
         integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
@@ -190,45 +211,6 @@ public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate
     }
 
 
-    @Override
-    public void initViews(View root) {
-        postValues = new ArrayList<>();
-        iv_qrcode= (ImageView) root.findViewById(R.id.qr_image);
-        vendor= (TextView) root.findViewById(R.id.qr_scan_vendor);
-        total= (TextView) root.findViewById(R.id.qr_total_value);
-        taxes = (TextView) root.findViewById(R.id.qr_scan_taxes_value);
-        itemsValue = (TextView) root.findViewById(R.id.qr_scan_items_value);
-        pb_create = (ProgressBar) root.findViewById(R.id.pb_create);
-        pay = (Button) root.findViewById(R.id.pay);
-        pay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(QRScanActivity.this, OnepocketPurchaseActivity.class);
-                Bundle bundle = new Bundle();
-                String data = "{\"businessName\":\"" + vendor.getText() +
-                                "\",\"purchaseValue\":\"" + total.getText().toString().replace("$", "").trim() +
-                                "\",\"taxValue\":\"" + taxes.getText().toString().replace("$", "").trim() +
-                                "\",\"devolutionBasisTax\":\"" + itemsValue.getText().toString().replace("$", "").trim() +
-                                "\",\"codeISO\":\"" + currency +
-                                "\",\"reference\":\"" + reference +
-                                "\"}";
-                OneTransaction transaction = new OneTransaction(data,
-                        OPKConstants.TYPE_QRCODE,
-                        ((VisaCheckoutApp)QRScanActivity.this.getApplication()).getIdSession(),
-                        Constants.getUser(QRScanActivity.this).nombre,
-                        Constants.getUser(QRScanActivity.this).apellido,
-                        Constants.getUser(QRScanActivity.this).email,
-                        ((VisaCheckoutApp) getApplication()).getRawPassword(),
-                        Constants.getUser(QRScanActivity.this).idCuenta);
-
-                bundle.putParcelable(OPKConstants.EXTRA_PAYMENT, transaction);
-                intent.putExtras(bundle);
-                startActivity(intent);
-
-                finish();
-            }
-        });
-    }
     private boolean checkLogin() {
         if(((VisaCheckoutApp)this.getApplication()).getIdSession()==null){
             Intent i =new Intent(QRScanActivity.this,LoginActivity.class);
@@ -237,11 +219,7 @@ public class QRScanActivity extends FrontBackAnimate implements FrontBackAnimate
         }
         return true;
     }
-    private void setWaitinUI(boolean b) {
-        if (b) pb_create.setVisibility(View.VISIBLE);
-        else pb_create.setVisibility(View.GONE);
 
-    }
 
 
 }
