f4d995048f95dae3485cf78c6b3a60d6a9e50cb3
serfarfan <serfar260@hotmail.com>
2016-07-25 10:12:56 -0500

    Advances

---
+++ app/src/main/java/com/allem/alleminmotion/visacheckout/ProofOfCoverageActivity.java
@@ -0,0 +1,14 @@
+package com.allem.alleminmotion.visacheckout;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProofOfCoverageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proof_of_coverage);
+    }
+}
