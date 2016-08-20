61ec1bc0fee9c717cac7f1f6ffd651148eedd8a3
lchui <lchui@iatai.com>
2016-08-19 23:04:50 -0700

    - Latest onepocket 1.0.3-340: ALGAN-193 - Fixed launch QRScanActivity first time after install crashing issue (> Android 6.0 only) - Fixed missing type Id, document Id in data bundle required for onepocket transaction

--- app/src/main/java/com/allegra/handyuvisa/MarketPlaceActivity.java
+++ app/src/main/java/com/allegra/handyuvisa/MarketPlaceActivity.java
@@ -12,10 +12,8 @@ import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.allegra.handyuvisa.models.AllemUser;
 import com.allegra.handyuvisa.utils.Constants;
 import com.allegra.handyuvisa.utils.Util;
-import com.allem.onepocket.model.OneTransaction;
 import com.allem.onepocket.utils.OPKConstants;
 
 public class MarketPlaceActivity extends FrontBackAnimate implements FrontBackAnimate.InflateReadyListener {
@@ -187,22 +185,7 @@ public class MarketPlaceActivity extends FrontBackAnimate implements FrontBackAn
 
     public void openOnePocket(){
         Intent intent = new Intent(MarketPlaceActivity.this, OnepocketPurchaseActivity.class);
-        Bundle bundle = new Bundle();
-        AllemUser user = Constants.getUser(this);
-        VisaCheckoutApp app = (VisaCheckoutApp) getApplication();
-        OneTransaction transaction = new OneTransaction();
-        transaction.add("jsonPayment", onePocketmessage);
-        transaction.add("type", OPKConstants.TYPE_MARKETPLACE);
-        transaction.add("sessionId", app.getIdSession());
-        transaction.add("first", user.nombre);
-        transaction.add("last", user.apellido);
-        transaction.add("userName", user.email);
-        transaction.add("docType", user.idType);
-        transaction.add("docId", user.idNumber);
-        transaction.add("rawPassword", app.getRawPassword());
-        transaction.add("idCuenta", Integer.toString(app.getIdCuenta()));
-
-        bundle.putParcelable(OPKConstants.EXTRA_PAYMENT, transaction);
+        Bundle bundle = Constants.createPurchaseBundle(Constants.getUser(this), onePocketmessage, OPKConstants.TYPE_MARKETPLACE, (VisaCheckoutApp) getApplication());
         intent.putExtras(bundle);
         startActivityForResult(intent, MarketPlaceActivity.REQUEST_ONEPOCKET_RETURN);
 
