cdd8bbe0e273992fc1c108902ff729cde818b744
serfarfan <sfarfan@iatai.com>
2016-09-28 17:16:22 -0500

    Fix testing = false in Constants.java

--- app/src/main/java/com/allegra/handyuvisa/utils/Constants.java
+++ app/src/main/java/com/allegra/handyuvisa/utils/Constants.java
@@ -49,7 +49,7 @@ public class Constants {
     //***************** EASY CHANGE BETWEEN TESTING AND PRODUCTION SYSTEM URLS *********************
 
     //** Parameter indicates mode: TESTING = false implies PRODUCTION mode **
-    public final static boolean TESTING = true;
+    public final static boolean TESTING = false;
     public final static boolean TESTING_AIR = false;
 
     public final static String DEMOURL="http://demo.allemmarket.com/allemmarketapp/index/login/";
