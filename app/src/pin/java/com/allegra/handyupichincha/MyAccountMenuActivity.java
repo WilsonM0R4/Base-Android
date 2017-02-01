244033339973f46da3d814a344ac400a6fd3b7da
juanchosandox90 <juansandoval222@gmail.com>
2017-02-01 14:19:42 -0500

    ProofOfCoverage_FixForOnepocket

--- app/src/pin/java/com/allegra/handyupichincha/MyAccountMenuActivity.java
+++ app/src/pin/java/com/allegra/handyupichincha/MyAccountMenuActivity.java
@@ -61,7 +61,7 @@ public class MyAccountMenuActivity extends FrontBackAnimate implements FrontBack
         final Integer[] images = {R.drawable.menu__profile, R.drawable.my_benefits,R.drawable.menu_tips, R.drawable.coverage, R.drawable.menu__history,
                 R.drawable.legal2};
         final Class[] activities = {MyAccountActivity.class, com.allegra.handyuvisa.MyBenefits.class, com.allegra.handyuvisa.MyTips.class,
-                ProofOfCoverageActivity.class, OneTransactionsActivity.class, LegalActivity.class};
+                ProofOfCoverageDinamicoActivity.class, OneTransactionsActivity.class, LegalActivity.class};
         lv.setAdapter(new ArrayAdapter<String>(MyAccountMenuActivity.this, R.layout.profile_layout, names) {
 
             public View getView(final int position, View view, ViewGroup parent) {
@@ -71,57 +71,8 @@ public class MyAccountMenuActivity extends FrontBackAnimate implements FrontBack
                 rowView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if (position == 3) {
-                            if (Connectivity.isConnected(ctx) || Connectivity.isConnectedWifi(ctx) || Connectivity.isConnectedMobile(ctx)) {
-                               // Log.e("CON CONEXION: "," ESTOY AQUI");//Test 2: Succeded
-                                if (((com.allegra.handyuvisa.VisaCheckoutApp) ctx.getApplicationContext()).getIdSession() == null) {
-                                   // Log.e("CON CONEXION: "," NO ME HE LOGEADO");
-                                    setGetYourCertificateLayout();
-                                } else {
-                                   // Log.e("CON CONEXION: "," ESTOY LOGEADO");//Test 2: Succeded
-                                    Intent i = new Intent(ctx, ProofOfCoverageDinamicoActivity.class);
-                                    startActivity(i);
-                                }
-                            } else {
-                               // Log.e("SIN CONEXION: "," ESTOY AQUI");//Test 1: Succeded //Test 3: Succeded
-                                if (((com.allegra.handyuvisa.VisaCheckoutApp) ctx.getApplicationContext()).getIdSession() == null) {
-                                   // Log.e("SIN CONEXION: "," NO ESTOY LOGEADO");//Test 1: Succeded
-                                    setGetYourCertificateLayout();
-                                } else {
-                                  //  Log.e("SIN CONEXION: "," ESTOY LOGEADO");//Test 3: Succeded
-                                    //Toast.makeText(ctx, "NO TENGO INTERNET Y YA ME HE LOGEADO ACA LLAMO LA BASE DE DATOS", Toast.LENGTH_SHORT).show();
-                                    usuarioSQLiteHelper = new UsuarioSQLiteHelper(getApplicationContext());
-                                    db = usuarioSQLiteHelper.getReadableDatabase();
-                                    cursor = usuarioSQLiteHelper.getInformationDatabase(db);
-                                    if (cursor.moveToFirst()) {
-                                        do {
-                                            nombre = cursor.getString(0);
-                                            apellido = cursor.getString(1);
-                                            tipoid = cursor.getString(2);
-                                            numid = cursor.getString(3);
-                                            nummcard = cursor.getString(4);
-                                            value1 = cursor.getString(5);
-                                            value2 = cursor.getString(6);
-                                            value3 = cursor.getString(7);
-                                        } while (cursor.moveToNext());
-                                    }
-                                    Intent intent = new Intent(ctx, ProofOfCoverageActivityOff.class);
-                                    intent.putExtra("nombre", nombre);
-                                    intent.putExtra("apellido", apellido);
-                                    intent.putExtra("tipoid", tipoid);
-                                    intent.putExtra("numid", numid);
-                                    intent.putExtra("numcard", nummcard);
-                                    intent.putExtra("value1", value1);
-                                    intent.putExtra("value2", value2);
-                                    intent.putExtra("value3", value3);
-                                    startActivity(intent);
-                                }
-
-                            }
-                        } else {
-                            Intent intent = new Intent(MyAccountMenuActivity.this, activities[position]);
-                            MyAccountMenuActivity.this.startActivity(intent);
-                        }
+                        Intent intent = new Intent(MyAccountMenuActivity.this, activities[position]);
+                        MyAccountMenuActivity.this.startActivity(intent);
                     }
 
                 });
