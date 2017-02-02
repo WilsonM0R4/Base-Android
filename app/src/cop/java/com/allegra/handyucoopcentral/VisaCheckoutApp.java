8e7900a1095b5e2c66ecab4fd95a4963f01cb5d8
Gang Chen <mrcodingfarmer@gmail.com>
2017-02-01 18:34:32 -0800

    - Updated onepocket version 2.0.0-383 - Added CoopCentral Theme - Failure Purchase Dialog Format/Logic Change

--- app/src/cop/java/com/allegra/handyucoopcentral/VisaCheckoutApp.java
+++ app/src/cop/java/com/allegra/handyucoopcentral/VisaCheckoutApp.java
@@ -8,6 +8,7 @@ import android.util.Log;
 
 import com.allegra.handyuvisa.utils.Constants;
 import com.allem.onepocket.utils.OPKLibraryConfig;
+import com.allem.onepocket.utils.ThemeType;
 import com.urbanairship.UAirship;
 import com.urbanairship.push.notifications.DefaultNotificationFactory;
 
@@ -100,6 +101,7 @@ public class VisaCheckoutApp extends MultiDexApplication {
         new OPKLibraryConfig.Builder()
                 .testMode(Constants.TESTING)
                 .idPortal(Constants.ID_PORTAL)
+                .setTheme(ThemeType.COOPCENTRAL)
                 .build();
     }
 
