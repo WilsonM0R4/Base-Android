a7c9e8154c5dd9ff7f22c8b7ffe5e2c443f31f6f
juanchosandox90 <juansandoval222@gmail.com>
2017-02-02 16:10:49 -0500

    CorpbancaFIXES

--- app/src/cbh/java/com/allegra/handyucorpbanca/VisaCheckoutApp.java
+++ app/src/cbh/java/com/allegra/handyucorpbanca/VisaCheckoutApp.java
@@ -159,7 +159,10 @@ public class VisaCheckoutApp extends MultiDexApplication {
     }
 
     private void initOnepocket() {
-        new OPKLibraryConfig.Builder().testMode(Constants.TESTING).idPortal(Constants.ID_PORTAL).setTheme(ThemeType.PICHINCHA).build();
+            new OPKLibraryConfig.Builder()
+                    .testMode(Constants.TESTING)
+                    .idPortal(Constants.ID_PORTAL)
+                    .build();
+        }
     }
 
-}
