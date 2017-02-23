9ed5495e04a92ff8a538d98dc7675546801539e7
serfarfan <sfarfan@iatai.com>
2017-02-23 16:22:08 -0500

    In order to fix a Splunk:  Was lost the reference in the otto event bus object, because was final.

--- app/src/main/java/com/allegra/handyuvisa/async/MyBus.java
+++ app/src/main/java/com/allegra/handyuvisa/async/MyBus.java
@@ -8,7 +8,7 @@ import com.squareup.otto.Bus;
  */
 public class MyBus {
 
-    private static final Bus BUS = new Bus();
+    private static Bus BUS = new Bus();
 
     public static Bus getInstance() {
         return BUS;
